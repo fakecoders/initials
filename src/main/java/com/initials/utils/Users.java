6d4f0dc6cff46b0e51839b1b64888659ec16b535
lalitbodana <lalitbodana@gmail.com>
2020-09-01 20:34:55 +0530

    initails code

---
+++ src/main/java/com/initials/utils/Users.java
@@ -0,0 +1,13 @@
+package com.initials.utils;
+
+public class Users {
+	private int id;
+	
+	private String name;
+	
+	public static void main(String[] args) {
+		Users u = new Users();
+		System.out.println(u);
+
+}
+}
